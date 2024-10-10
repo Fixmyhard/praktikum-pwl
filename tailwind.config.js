700a7bee249b261d7ecf4e0393dfecd41f1747a2
Darius <dariusdarren86@gmail.com>
2024-10-10 17:00:28 +0700

    hehe

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+    comic: ['Comic Sans MS'],
+  },
+    extend: {},
+  },
+  plugins: [],
+}
+
